16385b4cfcebc7e18e5c17503100d4f616c5533c
Maxim <puper-1981@mail.ru>
2022-02-19 16:38:19 +0500

    Функции

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function getRandomNum(min, max) {
+  return Math.round(Math.random() * (max - min));
+}
+
+
+function checkLengthString(x, max) {
+  if (x <= max.length) {
+    return true;
+  }
+  return false;
+
+  console.log(2, 5);
+}
+
