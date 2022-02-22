dba1a829c1990f9465a58eff0349311540a914e1
Maxim <puper-1981@mail.ru>
2022-02-22 21:54:16 +0500

    Функции

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ function getRandomNum(min, max) {
   return Math.round(Math.random() * (max - min));
 }
 
+getRandomNum(2, 10);
 
-function checkLengthString(x, max) {
-  if (x <= max.length) {
+function checkLengthString(str, maxlength) {
+  if (str.length <= maxlength) {
     return true;
   }
   return false;
 }
 
+checkLengthString('advadfv', 20);
