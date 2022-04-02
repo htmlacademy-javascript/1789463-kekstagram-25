dddc6dd3976ea193383090043574edb64f64704a
Maxim <puper-1981@mail.ru>
2022-04-02 19:50:08 +0500

    Отрисуй меня полностью 2

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,22 @@
 import './photo.js';
 
 const bigPicture = document.querySelector('.big-picture');
+const bigPicturesImg = document.querySelector('.big-picture__img');
 const commentsLoader = document.querySelector('.comments-loader');
+const description = bigPicture.querySelector('.social__caption');
 const socialCommentCount = document.querySelector('.social__comment-count');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
 const pictureImg = document.querySelector('.picture__img');
 
 
-pictureImg.onclick = function () {
+pictureImg.onclick = function (photo) {
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
   socialCommentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
+  bigPicturesImg.querySelector('.img').src = photo.url;
+  bigPicturesImg.querySelector('.likes-count').textContent = `${photo.likes}`;
+  description.textContent = `${photo.description}`;
 };
 
 bigPictureCancel.onclick = function () {
