9d152811a3186913a165cac73822908c2447926b
Keks <keks@htmlacademy.ru>
2022-04-04 18:32:40 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/photo.js
+++ 6/js/photo.js
@@ -6,17 +6,24 @@ const getPictureTemplate = document.querySelector('#picture')
   .querySelector('.picture');
 
 const pictureCase = getPhoto();
-const pictureFragment = document.createDocumentFragment();
 
-pictureCase.forEach(({comments, likes, url}) => {
-  const pictureElement = getPictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureFragment.appendChild(pictureElement);
-});
+const renderSimilarList = () => {
+  const pictureFragment = document.createDocumentFragment();
+
+  pictureCase.forEach(({comments, likes, url}) => {
+    const pictureElement = getPictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureFragment.appendChild(pictureElement);
+  });
+
+  getPictures.appendChild(pictureFragment);
+};
+
+const clearSimilarList = () => {
+  getPictures.innerHTML = '';
+};
 
 
-getPictures.appendChild(pictureFragment);
-
-window.console.log(getPictures);
+export {renderSimilarList, clearSimilarList};
--- 6/js/popup.js
+++ 6/js/popup.js
@@ -1,30 +1,62 @@
-import './photo.js';
+import { clearSimilarList, renderSimilarList } from './photo.js';
+import { isEscapeKey, isEnterKey } from './util.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPicturesImg = document.querySelector('.big-picture__img');
 const commentsLoader = document.querySelector('.comments-loader');
-const description = bigPicture.querySelector('.social__caption');
+// const description = bigPicture.querySelector('.social__caption');
 const socialCommentCount = document.querySelector('.social__comment-count');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
-const pictureImg = document.querySelector('.picture__img');
+// const pictureImg = document.querySelector('.picture__img');
 
 
-pictureImg.onclick = function (photo) {
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+function openModal() {
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
   socialCommentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
-  bigPicturesImg.querySelector('.img').src = photo.url;
-  bigPicturesImg.querySelector('.likes-count').textContent = `${photo.likes}`;
-  description.textContent = `${photo.description}`;
-};
+  renderSimilarList();
 
-bigPictureCancel.onclick = function () {
+  document.addEventListener('keydown', onPopupEscKeydown);
+}
+
+
+function closeModal() {
   bigPicture.classList.add('hidden');
-};
+  clearSimilarList();
+  document.removeEventListener('keydown', onPopupEscKeydown);
+}
 
-document.addEventListener('keydown', (evt) => {
-  if(evt.keyCode === 27) {
-    bigPicture.classList.add('hidden');
+bigPicturesImg.addEventListener('click', () => {
+  openModal();
+});
+
+bigPicturesImg.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    openModal();
   }
 });
+
+bigPictureCancel.addEventListener('click', () => {
+  closeModal();
+});
+
+bigPictureCancel.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    closeModal();
+  }
+});
+
+
+/* document.addEventListener('keydown', () => {
+  if(isEscapeKey === 27) {
+    bigPicture.classList.add('hidden');
+  }
+}); */
