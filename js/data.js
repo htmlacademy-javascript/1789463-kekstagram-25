0c072d62fdb137765c78f66cc87c32519e9b120f
Maxim <puper-1981@mail.ru>
2022-04-09 17:13:42 +0500

    Правда или действие 1

--- js/data.js
+++ js/data.js
@@ -1,64 +1,69 @@
-import {getRandomNum, getRandomArrayElement} from './util.js';
+import { getRandomNumber, getRandomItem } from './util.js';
 
-const NAMES = [
-  'Артем',
-  'Богдан',
-  'Владислав',
-  'Георгий',
-  'Дмитрий',
-  'Егор',
-];
-
-const DESCRIPTIONS = [
-  'УРА!',
-  'Отдых',
-  'Непонятно',
-  'Эврика',
-  'ЧТО???',
-];
-
-const MESSAGES = [
+const MESSAGES_SET = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+const NAMES = [
+  'Александр',
+  'Адрей',
+  'Владимир',
+  'Вернон',
+  'Владислав',
+  'Григорий',
+  'Дмитрий',
+  'Емеля',
+  'Елисей',
+  'Житомир',
+  'Тарас',
+  'Николай',
+];
 
-const createComments = () => {
-  const randomAvatar = getRandomNum(0, 6);
-  const randomIdComment = getRandomNum(1, 1000);
+const DESCRIPTIONS = [
+  'Просто фото',
+  'Хорошо',
+  'Класс!',
+  'Великолепная фотография',
+];
 
-  return {
-    id: randomIdComment,
-    avatar: `img/avatar-${randomAvatar}.svg`,
-    message: getRandomArrayElement(MESSAGES),
-    name: getRandomArrayElement(NAMES),
-  };
-};
+const commentAmount = 16;
+const createComments = (photoNumber) => {
+  const comments = [];
 
-const createDescriptionPhoto = (index) => {
-  const randomLikes = getRandomNum(15, 200);
-  const randomId =  getRandomNum(1, 25);
-
-  return{
-    id: randomId,
-    url: `photos/${index}.jpg`,
-    description: getRandomArrayElement(DESCRIPTIONS),
-    likes: randomLikes,
-    comments: createComments(getRandomNum(0, 6)),
-  };
-};
-
-const getPhoto = () => {
-  const photo = [];
-  for (let i = 1; i <=25; i++) {
-    photo.push(createDescriptionPhoto(i));
+  for (let i = 1; i <= commentAmount; i++) {
+    const comment = {
+      id: photoNumber + i / 10,
+      avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+      message: getRandomItem(MESSAGES_SET),
+      name: getRandomItem(NAMES),
+    };
+    comments.push(comment);
   }
 
-  return photo;
+  return comments;
 };
 
-export {getPhoto};
+const createPhoto = (photoNumber) => ({
+  id: photoNumber,
+  url: `photos/${photoNumber}.jpg`,
+  description: getRandomItem(DESCRIPTIONS),
+  likes: getRandomNumber(15, 200),
+  comments: createComments(photoNumber),
+});
+
+const createPhotos = (count) => {
+  const photos = [];
+
+  for (let i = 1; i <= count; i++) {
+    photos.push(createPhoto(i));
+  }
+
+  return photos;
+};
+
+export { createPhotos };
--- js/photo.js
+++ js/photo.js
@@ -1,22 +1,91 @@
-import {getPhoto} from './data.js';
+import { isEscapeKey } from './util.js';
 
-const getPictures = document.querySelector('.pictures');
-const getPictureTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('.picture');
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
+const likesCount = bigPicture.querySelector('.likes-count');
+const socialComments = bigPicture.querySelector('.social__comments');
+const commentTemplate = bigPicture.querySelector('.social__comment');
+const socialCaption = bigPicture.querySelector('.social__caption');
+const socialCommentCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
 
-const pictureCase = getPhoto();
-const pictureFragment = document.createDocumentFragment();
+const COMMENTS_BLOCK = 5;
 
-pictureCase.forEach(({comments, likes, url}) => {
-  const pictureElement = getPictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureFragment.appendChild(pictureElement);
-});
+let commentsToRenderCount = COMMENTS_BLOCK;
+let commentsArray;
 
+const renderComments = () => {
+  if (commentsArray.length <= commentsToRenderCount) {
+    commentsToRenderCount = commentsArray.length;
+    commentsLoader.classList.add('hidden');
+  }
 
-getPictures.appendChild(pictureFragment);
+  socialCommentCount.textContent = `${commentsToRenderCount} из ${commentsArray.length} комментариев`;
 
-window.console.log(getPictures);
+  const commentsToShow = commentsArray.slice(0, commentsToRenderCount);
+
+  const fragment = document.createDocumentFragment();
+
+  commentsToShow.forEach((comment) => {
+    const socialComment = commentTemplate.cloneNode(true);
+    const commentAvatar = socialComment.querySelector('.social__picture');
+    const commentMessage = socialComment.querySelector('.social__text');
+
+    commentAvatar.src = comment.avatar;
+    commentAvatar.alt = comment.name;
+    commentMessage.textContent = comment.message;
+    fragment.appendChild(socialComment);
+  });
+
+  socialComments.innerHTML = '';
+  socialComments.appendChild(fragment);
+
+  commentsToRenderCount += COMMENTS_BLOCK;
+};
+
+const closeFullSizeImage = (listener) => {
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', listener);
+  commentsLoader.removeEventListener('click', renderComments);
+
+  commentsToRenderCount = COMMENTS_BLOCK;
+  commentsLoader.classList.remove('hidden');
+};
+
+const onImageEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFullSizeImage(onImageEscKeydown);
+  }
+};
+
+const onCloseButtonClick = () => {
+  closeFullSizeImage(onImageEscKeydown);
+};
+
+const openFullSizeImage = () => {
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onImageEscKeydown);
+  commentsLoader.addEventListener('click', renderComments);
+};
+
+const createFullSizeImage = (photo) => {
+  const { url, description, likes, comments } = photo;
+  bigPictureImage.src = url;
+  likesCount.textContent = likes;
+  socialCaption.textContent = description;
+
+  commentsArray = comments;
+
+  renderComments();
+  openFullSizeImage();
+};
+
+closeButton.addEventListener('click', onCloseButtonClick);
+
+export { createFullSizeImage, onCloseButtonClick };
