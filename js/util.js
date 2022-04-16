954958e0af5d3e90776985e1d3413c21f800533c
Maxim <puper-1981@mail.ru>
2022-04-17 02:28:53 +0500

    перламутровые пуговиц

--- js/util.js
+++ js/util.js
@@ -3,4 +3,24 @@ const getRandomItem = (items) => items[getRandomNumber(0, items.length - 1)];
 const checkLength = (string, maxLength) => string.length <= maxLength;
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export { getRandomNumber, checkLength, getRandomItem, isEscapeKey };
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export { getRandomNumber, checkLength, getRandomItem, isEscapeKey, debounce, throttle };
